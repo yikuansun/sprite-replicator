7cb0e87f9daef16a0c1c519f0a95c30c5efca9e8
yikuansun <yikuansun2015@gmail.com>
2022-02-02 22:27:51 -0500

    Add a sample function
    to be edited later

--- code.js
+++ code.js
@@ -1,3 +1,30 @@
 var canv = oCanvas.create({
     canvas: document.querySelector("canvas")
 });
+
+var draw = function() {
+    canv.clear();
+    var og_sprite = canv.display.image({
+        x: 144,
+        y: 144,
+        origin: { x: "center", y: "center" },
+        image: "https://i.picsum.photos/id/1064/200/200.jpg?hmac=xUH-ovzKEHg51S8vchfOZNAOcHB6b1TI_HzthmqvcWU"
+    });
+    canv.addChild(og_sprite);
+
+    // scale
+    og_sprite.scale(2, 2);
+
+    // offset
+    og_sprite.move(200, 200);
+
+    // rotate
+    og_sprite.rotate(20);
+
+    // duplicate
+    var sampleduplicate = og_sprite.clone({
+        x: 0,
+        y: 0
+    });
+    canv.addChild(sampleduplicate);
+};
