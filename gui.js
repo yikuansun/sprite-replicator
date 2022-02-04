7d3e4b828a752b654d08ea00270e382b8b3fa895
yikuansun <yikuansun2015@gmail.com>
2022-02-03 21:19:56 -0500

    Start making gui building function

---
+++ gui.js
@@ -0,0 +1,35 @@
+function buildGUIsection(fields, container=document.body) {
+    var table = document.createElement("table");
+    container.appendChild(table);
+    table.style.width = "100%";
+
+    for (var field of fields) {
+        var tr = document.createElement("tr");
+        table.appendChild(tr);
+
+        var fieldLabelTD = document.createElement("td");
+        fieldLabelTD.innerHTML = `<label for="${field.id}">${field.label}</label>`;
+        tr.appendChild(fieldLabelTD);
+
+        var fieldInputTD = document.createElement("td");
+        fieldInputTD.style.width = "1px"; // force right align
+        var inputElem = document.createElement("input");
+        inputElem.id = field.id;
+        inputElem.type = field.type;
+        fieldInputTD.appendChild(inputElem);
+        tr.appendChild(fieldInputTD);
+    }
+}
+
+buildGUIsection([
+    {
+        label: "i value",
+        id: "iValueInput",
+        type: "text"
+    },
+    {
+        label: "j value",
+        id: "jValueInput",
+        type: "text"
+    }
+]);
