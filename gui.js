94a54014a10ec3c33d70ce6f8f596497b16f27e4
yikuansun <yikuansun2015@gmail.com>
2022-02-06 15:35:54 -0500

    Use ygui.js

--- gui.js
+++ gui.js
@@ -1,51 +1,4 @@
-function buildGUIsection(fields, container=document.body) {
-    var inputs_out = [];
-
-    var table = document.createElement("table");
-    container.appendChild(table);
-    table.style.width = "100%";
-    table.className = "ygui-table";
-
-    for (var field of fields) {
-        var tr = document.createElement("tr");
-        table.appendChild(tr);
-
-        var fieldLabelTD = document.createElement("td");
-        fieldLabelTD.innerHTML = `<label for="${field.id}" class="ygui-label">${field.label}</label>`;
-        tr.appendChild(fieldLabelTD);
-
-        var fieldInputTD = document.createElement("td");
-        fieldInputTD.style.width = "1px"; // force right align
-        switch (field.type) {
-            case "select":
-                var inputElem = document.createElement("select");
-                for (var option of field.options) {
-                    var optionElem = document.createElement("option");
-                    optionElem.innerHTML = option;
-                    inputElem.appendChild(optionElem);
-                }
-                break;
-            default:
-                var inputElem = document.createElement("input");
-                inputElem.type = field.type;
-                break;
-        }
-        inputElem.id = field.id;
-        inputElem.className = "ygui-input";
-        fieldInputTD.appendChild(inputElem);
-        tr.appendChild(fieldInputTD);
-
-        for (var attribute in field.attr) {
-            inputElem.setAttribute(attribute, field.attr[attribute]);
-        }
-
-        inputs_out.push(inputElem);
-    }
-
-    return inputs_out;
-}
-
-var baseInputs = buildGUIsection([
+var baseInputs = ygui.buildGUIsection([
     {
         label: "X position",
         id: "og_x",
@@ -83,7 +36,7 @@ var baseInputs = buildGUIsection([
         attr: { value: 0.69, min: 0, max: 1, step: 0.01 }
     }
 ], document.querySelector("#base-section"));
-var duplicatesInputs = buildGUIsection([
+var duplicatesInputs = ygui.buildGUIsection([
     {
         label: "Number of duplicates",
         id: "duplicates",
